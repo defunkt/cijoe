b6e134098ea411a0437ccc1889f823b21c77f210
Paul Battley <pbattley@gmail.com>
2009-10-14 17:44:16 +0800

    More concise (and more efficient) code through the magic of regular expressions.

--- lib/cijoe/server.rb
+++ lib/cijoe/server.rb
@@ -37,13 +37,7 @@ class CIJoe
       # thanks integrity!
       def bash_color_codes(string)
         string.gsub("\e[0m", '</span>').
-          gsub("\e[31m", '<span class="color31">').
-          gsub("\e[32m", '<span class="color32">').
-          gsub("\e[33m", '<span class="color33">').
-          gsub("\e[34m", '<span class="color34">').
-          gsub("\e[35m", '<span class="color35">').
-          gsub("\e[36m", '<span class="color36">').
-          gsub("\e[37m", '<span class="color37">')
+          gsub(/\e\[(\d+)m/, "<span class=\"color\\1\">")
       end
 
       def pretty_time(time)
