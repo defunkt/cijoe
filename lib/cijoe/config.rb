3e059632cb15e4038c999f4993d30ba7dc235efd
Antono Vasiljev <antono.vasiljev@gmail.com>
2010-02-21 18:05:28 +0800

    Fixed tests broken by ec1677a
     * project_path initialization in Config
       take global (cmdline) or use self as default

--- lib/cijoe/config.rb
+++ lib/cijoe/config.rb
@@ -5,8 +5,9 @@ class CIJoe
     end
 
     def initialize(command, parent = nil)
-      @command = command
-      @parent = parent
+      @command  = command
+      @parent   = parent
+      @project_path = $project_path || File.join(File.dirname(__FILE__), '../../')
     end
 
     def method_missing(command, *args)
@@ -14,31 +15,29 @@ class CIJoe
     end
 
     def to_s
-      Dir.chdir($project_path) do
-        git_command = "git config #{config_string}"
-        result = `#{git_command} 2>&1`.chomp
-        process_status = $?
-      
-        if successful_command?(process_status) || config_command_with_empty_value?(result,process_status)
-          return result
-        else
-          raise "Error calling git config, is a recent version of git installed? Command: #{git_command}, Error: #{result}"
-        end
+      git_command = "cd #{@project_path} && git config #{config_string}"
+      result = `#{git_command} 2>&1`.chomp
+      process_status = $?
+
+      if successful_command?(process_status) || config_command_with_empty_value?(result,process_status)
+        return result
+      else
+        raise "Error calling git config, is a recent version of git installed? Command: #{git_command}, Error: #{result}"
       end
     end
 
     def config_string
       @parent ? "#{@parent.config_string}.#{@command}" : @command
     end
-    
+
     private
-    
+
     def successful_command?(process_status)
       process_status.exitstatus.to_i == 0
     end
-    
+
     def config_command_with_empty_value?(result, process_status)
       process_status.exitstatus.to_i == 1 && result.empty?
-    end    
+    end
   end
 end
